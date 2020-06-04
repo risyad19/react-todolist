524ed1f6406d2fcebca0ab539bc161501280c7e8
risyad19 <57974857+risyad19@users.noreply.github.com>
2020-06-04 16:00:00 +0700

    Membuat todoform toggle

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const TodoForm = ({ addTodo }) => {
+const TodoForm = ({ addTodo, showAdd }) => {
   const [value, setValue] = useState("");
 
   const handleFormSubmit = e => {
@@ -17,24 +17,30 @@ const TodoForm = ({ addTodo }) => {
     setValue("");
   };
 
+  if (showAdd) {
+    return (
+      <section className="add">
+        <form className="add-form" onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            className="add-input"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
+          <button className="add-btn main-black-color">Add</button>
+        </form>
+      </section>
+    );
+  } else {
+    return null;
+  }
+
   // console.log("value", value);
-  return (
-    <section className="add">
-      <form className="add-form" onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          className="add-input"
-          value={value}
-          onChange={e => setValue(e.target.value)}
-        />
-        <button className="add-btn main-black-color">Add</button>
-      </form>
-    </section>
-  );
 };
 
 TodoForm.propTypes = {
-  addTodo: PropTypes.func.isRequired
+  addTodo: PropTypes.func.isRequired,
+  showAdd: PropTypes.bool.isRequired
 };
 
 export default TodoForm;
